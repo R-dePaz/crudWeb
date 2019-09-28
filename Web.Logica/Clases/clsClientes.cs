1c9bd17027823a61eb2f367e38c506355421e021
Rudy de Paz <depazrg@gmail.com>
2019-09-28 13:35:50 -0600

    crud

---
+++ Web.Logica/Clases/clsClientes.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web.Logica.Clases
+{
+    public class clsClientes
+    {
+        string stConexion = "";
+        SqlCommand sqlCommand = null;
+        SqlConnection sqlConnection = null;
+        SqlParameter sqlParameter = null;
+        SqlDataAdapter sqlDataAdapter = null;
+
+        public clsClientes()
+        {
+            clsConexion obclsConexion = new clsConexion();
+            stConexion = obclsConexion.stGetConexion();
+        }
+
+        public DataSet stConsultarClientes(long lnIdentificacion)
+        {
+            try
+            {
+                DataSet dsConsulta = new DataSet();
+
+                sqlConnection = new SqlConnection(stConexion);
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand("spConsultarClientes", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add(new SqlParameter("@nIdentificacion", lnIdentificacion));
+
+                sqlCommand.ExecuteNonQuery();
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(dsConsulta);
+
+                return dsConsulta;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
+
+        public string stInsertarClientes(long lnIdentificacion,
+                                            string stNombres,
+                                            string stApellidos)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(stConexion);
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand("spInsertarClientes", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add(new SqlParameter("@nIdentificacion", lnIdentificacion));
+                sqlCommand.Parameters.Add(new SqlParameter("@cNombres", stNombres));
+                sqlCommand.Parameters.Add(new SqlParameter("@cApellidos", stApellidos));
+
+                sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@cMensaje";
+                sqlParameter.SqlDbType = SqlDbType.VarChar;
+                sqlParameter.Size = 100;
+                sqlParameter.Direction = ParameterDirection.Output;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+                sqlCommand.ExecuteNonQuery();
+
+                return sqlParameter.Value.ToString();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
+
+        public string stModificarClientes(long lnIdentificacion,
+                                            string stNombres,
+                                            string stApellidos)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(stConexion);
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand("spModificarClientes", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add(new SqlParameter("@nIdentificacion", lnIdentificacion));
+                sqlCommand.Parameters.Add(new SqlParameter("@cNombres", stNombres));
+                sqlCommand.Parameters.Add(new SqlParameter("@cApellidos", stApellidos));
+
+                sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@cMensaje";
+                sqlParameter.SqlDbType = SqlDbType.VarChar;
+                sqlParameter.Size = 100;
+                sqlParameter.Direction = ParameterDirection.Output;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+                sqlCommand.ExecuteNonQuery();
+
+                return sqlParameter.Value.ToString();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
+
+        public string stEliminarClientes(long lnIdentificacion)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(stConexion);
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand("spEliminarClientes", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add(new SqlParameter("@nIdentificacion", lnIdentificacion));
+
+                sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@cMensaje";
+                sqlParameter.SqlDbType = SqlDbType.VarChar;
+                sqlParameter.Size = 100;
+                sqlParameter.Direction = ParameterDirection.Output;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+                sqlCommand.ExecuteNonQuery();
+
+                return sqlParameter.Value.ToString();
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
+    }
+}
---
+++ Web/Interfaces/frmPrincipal.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+
+namespace Web.Interfaces
+{
+    public partial class frmPrincipal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMensaje.Text = "";
+                if (string.IsNullOrEmpty(txtIdentificacion.Text)) throw new Exception("Ingrese identificacion");
+
+                Logica.Clases.clsClientes obclsClientes = new Logica.Clases.clsClientes();
+                DataSet dsConsulta = obclsClientes.stConsultarClientes(Convert.ToInt64(txtIdentificacion.Text));
+
+                if (dsConsulta.Tables[0].Rows.Count == 0) gvwDatos.DataSource = null;
+                else gvwDatos.DataSource = dsConsulta;
+
+                gvwDatos.DataBind();
+            }
+            catch (Exception ex) { lblMensaje.Text = ex.Message; }
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMensaje.Text = "";
+                Logica.Clases.clsClientes obclsClientes = new Logica.Clases.clsClientes();
+
+                lblMensaje.Text = obclsClientes.stInsertarClientes(Convert.ToInt64(txtIdentificacion.Text),
+                    txtNombres.Text,
+                    txtApellidos.Text);
+            }
+            catch (Exception ex) { lblMensaje.Text = ex.Message; }
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMensaje.Text = "";
+                Logica.Clases.clsClientes obclsClientes = new Logica.Clases.clsClientes();
+
+                lblMensaje.Text = obclsClientes.stModificarClientes(Convert.ToInt64(txtIdentificacion.Text),
+                    txtNombres.Text,
+                    txtApellidos.Text);
+            }
+            catch (Exception ex) { lblMensaje.Text = ex.Message; }
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMensaje.Text = "";
+                Logica.Clases.clsClientes obclsClientes = new Logica.Clases.clsClientes();
+
+                lblMensaje.Text = obclsClientes.stEliminarClientes(Convert.ToInt64(txtIdentificacion.Text));
+            }
+            catch (Exception ex) { lblMensaje.Text = ex.Message; }
+        }
+    }
+}
